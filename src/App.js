78cd5f9ee857d5b83dcf59d630439caab5c874dc
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2022-12-22 00:26:10 -0300

    kk

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Teste de branch</p>
+        <p>Teste de branch 2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
